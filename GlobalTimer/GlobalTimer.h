f461d2f6f59ed829c2fc0cb1b8daab12c46c504b
mabinglei <mabinglei@hex-tech.net>
2022-11-18 11:13:03 +0800

    Initial Commit

---
+++ GlobalTimer/GlobalTimer.h
@@ -0,0 +1,18 @@
+//
+//  GlobalTimer.h
+//  GlobalTimer
+//
+//  Created by 马冰垒 on 2022/11/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GlobalTimer.
+FOUNDATION_EXPORT double GlobalTimerVersionNumber;
+
+//! Project version string for GlobalTimer.
+FOUNDATION_EXPORT const unsigned char GlobalTimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GlobalTimer/PublicHeader.h>
+
+
